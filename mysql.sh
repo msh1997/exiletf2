a264d4a2cf0475b306952f0733acd65dca63e6ed
Michael Shu <michaelwsx@gmail.com>
2021-05-14 04:13:17 -0400

    fixing mysql script

--- mysql.sh
+++ mysql.sh
@@ -2,7 +2,7 @@
 
 mysql -u root -p$MYSQL_ROOT_PASSWORD << EOF
 CREATE USER 'user'@'%' IDENTIFIED WITH mysql_native_password BY 'pass';
-GRANT ALL PRIVILEGES ON . TO 'user'@'%';
+GRANT ALL PRIVILEGES ON *.* TO 'user'@'%';
 FLUSH PRIVILEGES;
 CREATE DATABASE exiletf2;
 EOF
